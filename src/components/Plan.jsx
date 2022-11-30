52132c01263d61891de3d690d69be31495ed3759
David Vashakidze <83545222+vashaka@users.noreply.github.com>
2022-11-30 21:39:59 +0400

    added navbar sticky functionallity and plans basic structure

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -7,9 +7,8 @@ const styles = {
 };
 
 const Plan = ({
-  HotelTitle,
+  PlanTitle,
   HotelSelectedImage,
-  stars,
   HotelImages,
   priceOnOneDay,
   reviews,
@@ -17,90 +16,18 @@ const Plan = ({
 }) => {
   const [selectedimg, setSelectedImg] = React.useState(HotelSelectedImage);
 
-  const starsOfHotel = Number(stars);
   return (
     <div
-      className="cursor-pointer	md:hover:scale-105 ease-in duration-300 w-full h-[375px] object-cover"
+      className="cursor-pointer	md:hover:scale-105 ease-in duration-300 w-full h-[auto] object-cover"
       style={{
         borderRadius: "25px",
         border: "3px solid rgb(255, 255, 255)",
         boxShadow: "rgb(19 15 235 / 15%) 1px 2px 20px",
       }}
     >
-      <p className="">{HotelTitle}</p>
-      <div className="flex justify-center">
-        {starsOfHotel === 2 && (
-          <>
-            <RiStarSFill />
-            <RiStarSFill />
-          </>
-        )}
-        {starsOfHotel === 3 && (
-          <>
-            <RiStarSFill />
-            <RiStarSFill />
-            <RiStarSFill />
-          </>
-        )}
-        {starsOfHotel === 4 && (
-          <>
-            <RiStarSFill />
-            <RiStarSFill />
-            <RiStarSFill />
-            <RiStarSFill />
-          </>
-        )}
-        {starsOfHotel === 5 && (
-          <>
-            <RiStarSFill />
-            <RiStarSFill />
-            <RiStarSFill />
-            <RiStarSFill />
-            <RiStarSFill />
-          </>
-        )}
-      </div>
-      <div className="flex justify-center h-[200px]">
-        <img
-          src={selectedimg}
-          alt="Coolest Hotel"
-          width={350}
-          className="rounded-xl mt-2"
-        />
-      </div>
-      <div className="grid grid-flow-col gap-0 mt-1">
-        {HotelImages?.map((HotelImage) => (
-          <img
-            src={HotelImage.image}
-            width={70}
-            alt=""
-            className={styles.img}
-            onMouseEnter={() => setSelectedImg(HotelImage.image)}
-          />
-        ))}
-      </div>
-      <div className="flex justify-start mt-[3px] mx-4 md:mx-1">
-        <p className="text-sm text-gray-400">1 night, 2 adults</p>
-      </div>
-      <div className="flex justify-start -mt-1 mx-4 md:mx-1">
-        <p className="font-medium">USD {priceOnOneDay}</p>
-      </div>
-      <div className="flex justify-end -mt-[2.7rem] mx-4 md:mx-1">
-        <div className="">
-          <p>Fabulous</p>
-          <p className="text-[14px] mr-2 text-gray-400 -mt-1">
-            {reviews} reviews
-          </p>
-        </div>
-
-        <div
-          className="bg-[#003580] w-[32px] h-[32px] mt-[4px] flex justify-center items-center"
-          style={{ borderRadius: "5px 5px 10px 0px" }}
-        >
-          <p className="text-[#ededed] font-medium">{rating}</p>
-        </div>
-      </div>
-      <button className="">Add to Cart</button>
+      <p className="">{PlanTitle}</p>
+      <p>Locations</p>
+      <p className="opacity-30">What you'll see</p>
     </div>
   );
 };
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Plan from "../components/Plan";
+import Hotel from "../components/Hotel";
 import ImageSlider from "../components/ImageSlider";
 import Navbar from "../components/Navbar";
 import { SliderData } from "../data/SliderData";
 import { WyndhamBatumiImages } from "../data/HotelImages";
 import GEO_MAP from "../data/GEO_MAP.png";
+import Plan from "../components/Plan";
 
 const MainPage = () => {
   return (
@@ -13,15 +14,15 @@ const MainPage = () => {
       <div className="flex justify-center">
         <img src={GEO_MAP} alt="geo-map" width={800} />
       </div>
-      <div className="max-w-[1240px] m-auto py-16 px-4 text-center">
+      <div className="max-w-[1240px] m-auto py-16 px-4 text-center z-10">
         <h1 className="text-3xl md:text-4xl text-[#f51767]">
           Choose Your Plan
         </h1>
-        <p className="text-xl">Tap on your favourite</p>
+        <p className="text-xl">Tap on your favourite and see it on the map</p>
         <div className="grid grid-rows-none md:grid-cols-2 lg:grid-cols-3 py-4 gap-2 md:gap-4">
           <div className="w-full col-span-2 md:col-span-1 row-span-1 xl:col-span-1 relative">
             <Plan
-              HotelTitle="Wyndham Batumi"
+              PlanTitle="Standard"
               stars="5"
               HotelSelectedImage={WyndhamBatumiImages[0].image}
               HotelImages={WyndhamBatumiImages}
@@ -35,7 +36,7 @@ const MainPage = () => {
             className="w-full col-span-2 md:col-span-1 row-span-1 xl:col-span-1"
           >
             <Plan
-              HotelTitle="Kakheti Batumi"
+              PlanTitle="Premium"
               stars="3"
               HotelSelectedImage={WyndhamBatumiImages[0].image}
               HotelImages={WyndhamBatumiImages}
@@ -49,6 +50,54 @@ const MainPage = () => {
             className="w-full col-span-2 md:col-span-2 row-span-1 lg:col-span-1"
           >
             <Plan
+              PlanTitle="Premium"
+              stars="3"
+              HotelSelectedImage={WyndhamBatumiImages[0].image}
+              HotelImages={WyndhamBatumiImages}
+              priceOnOneDay="170"
+              reviews="1,341"
+              rating="8.6"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] m-auto py-16 px-4 text-center">
+        <h1 className="text-3xl md:text-4xl text-[#f51767]">
+          Choose Your Hotel
+        </h1>
+        <p className="text-xl">Tap on your favourite</p>
+        <div className="grid grid-rows-none md:grid-cols-2 lg:grid-cols-3 py-4 gap-2 md:gap-4">
+          <div className="w-full col-span-2 md:col-span-1 row-span-1 xl:col-span-1 relative">
+            <Hotel
+              HotelTitle="Wyndham Batumi"
+              stars="5"
+              HotelSelectedImage={WyndhamBatumiImages[0].image}
+              HotelImages={WyndhamBatumiImages}
+              priceOnOneDay="170"
+              reviews="1,341"
+              rating="8.6"
+            />
+          </div>
+          <div
+            // className="w-full h-[350px] object-cover col-span-1 md:col-span-1 row-span-1 lg:col-span-1"
+            className="w-full col-span-2 md:col-span-1 row-span-1 xl:col-span-1"
+          >
+            <Hotel
+              HotelTitle="Kakheti Batumi"
+              stars="3"
+              HotelSelectedImage={WyndhamBatumiImages[0].image}
+              HotelImages={WyndhamBatumiImages}
+              priceOnOneDay="170"
+              reviews="1,341"
+              rating="8.6"
+            />
+          </div>
+          <div
+            // className="w-full h-[350px] object-cover col-span-1 md:col-span-1 row-span-1 lg:col-span-1"
+            className="w-full col-span-2 md:col-span-2 row-span-1 lg:col-span-1"
+          >
+            <Hotel
               HotelTitle="Kakheti Batumi"
               stars="3"
               HotelSelectedImage={WyndhamBatumiImages[0].image}
